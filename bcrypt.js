e5298d3257fef23dc6db64f08a0b40dc8286c2b5
pagerme123 <122313567+pagerme123@users.noreply.github.com>
2023-08-08 21:36:17 +0100

    Update bcrypt.js

--- bcrypt.js
+++ bcrypt.js
@@ -1,1 +1,19 @@
-var e=prompt("Please enter the password:"),t="Bigballs@69";e!==t&&(console.log("Access denied! Incorrect password."),window.location.href="index.html");
+
+
+function verifyPassword() {
+    const enteredPassword = prompt("Please enter the password:");
+    const correctPassword = "Bigballs@69";
+
+    if (enteredPassword !== null) {
+        if (enteredPassword === correctPassword) {
+            console.log("Access granted! You entered the correct password.");
+        } else {
+            console.log("Access denied! Incorrect password.");
+            window.location.href = "index.html";
+        }
+    } else {
+        console.log("Access denied! No password entered.");
+    }
+}
+
+verifyPassword();
