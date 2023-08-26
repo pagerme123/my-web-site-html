5ac0ac7e1534cab54769ec1578e25c8a5d75069b
pagerme123 <122313567+pagerme123@users.noreply.github.com>
2023-08-26 20:05:36 +0100

    5.0
    5.0

--- bcrypt.js
+++ bcrypt.js
@@ -1,18 +1,23 @@
-
+function redirectToIndex() {
+    window.location.href = "index.html";
+}
 
 function verifyPassword() {
-    const enteredPassword = prompt("Please enter the password:");
     const correctPassword = "Bigballs@69";
 
-    if (enteredPassword !== null) {
-        if (enteredPassword === correctPassword) {
-            console.log("Access granted! You entered the correct password.");
-        } else {
-            console.log("Access denied! Incorrect password.");
-            window.location.href = "index.html";
-        }
-    } else {
+    // Prompt the user for a password
+    const enteredPassword = prompt("Please enter the password:");
+
+    // Handle different cases
+    if (enteredPassword === null) {
         console.log("Access denied! No password entered.");
+        redirectToIndex();
+    } else if (enteredPassword === correctPassword) {
+        console.log("Access granted! You entered the correct password.");
+        // Continue with the rest of your code here
+    } else {
+        console.log("Access denied! Incorrect password.");
+        redirectToIndex();
     }
 }
 
